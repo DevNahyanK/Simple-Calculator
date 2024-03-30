f1763d523c96ccd322159b6b9c58f0fd09cc4508
DevNahyanK <nahyangaming789@gmail.com>
2024-03-31 03:07:36 +0500

    first

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Welcome message
+console.log("\n\tWelcome To \"Nahyan\", CLI Simple Calculator\n ")
+
+// Questions from User
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select the one Operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+// Conditional Statement
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Please Select Valid Operator");
+}
